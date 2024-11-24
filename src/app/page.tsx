4fb8e187913698a323c5fd0c36f1cf8323698f6e
dhemjitha <dhemjitha@gmail.com>
2024-11-24 19:07:47 +0530

    fixed: usestate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,33 @@
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [inputtext, setinputtext] = useState("");
 
   const handleClick = (param) => {
     alert(param);
   }
 
+  const handleChange = (e) => {
+    setinputtext(e.target.value);
+  }
+
   return (
-    <div>
+    <>
+      <Input type="text" onChange={handleChange} />
       <Button onClick={() => handleClick("Heyyy")}>Click Me</Button>
-    </div>
+      <p>Value is : {inputtext}</p>
+      <br />
+
+      {inputtext == "NextJS" && <div>
+        <h1>Members Area</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae labore a iste, sapiente esse fugiat quam, veritatis amet cum dignissimos debitis ipsa eligendi provident adipisci quod, placeat assumenda. Error, dolorum.</p>
+      </div>}
+      
+    </>
   );
 }
